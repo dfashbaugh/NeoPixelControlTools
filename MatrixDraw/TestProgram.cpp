06d35f354ff2b25dee34f12a8b9a367f1b9a75b5
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-11-13 14:23:11 -0500

    Add basic files for draw library

---
+++ MatrixDraw/TestProgram.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct ExpectedValue
+{
+	std::string valueName;
+	int expectedValue;
+	int actualValue;
+};
+
+void OnSuccess(std::string testName)
+{
+	cout << testName << ": " << "Passed" << endl;
+}
+
+void OnFailure(std::string testName)
+{
+	cout << testName << ": " << "Failed" << endl;
+}
+
+void ListExpected(ExpectedValue value)
+{
+	cout << "    " << value.valueName << " - Expected: " << value.expectedValue << " Found: " << value.actualValue << endl;
+}
+
+void CheckResults(std::string testName, ExpectedValue valueList [], int numberOfValues)
+{
+	for(int i = 0; i < numberOfValues; i++)
+	{
+		if(valueList[i].expectedValue != valueList[i].actualValue)
+		{
+			OnFailure(testName);
+
+			for(int j = 0; j < numberOfValues; j++)
+			{
+				ListExpected(valueList[j]);
+			}
+
+			return;
+		}
+	}
+
+	OnSuccess(testName);
+}
+
+int main(void)
+{
+	cout << "Test Beginning" << endl;
+
+	return 0;
+}
