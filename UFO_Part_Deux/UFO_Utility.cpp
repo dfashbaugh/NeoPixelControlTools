f83ad0e325ebed009ba8928b381a5b1591fff637
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2018-02-22 01:36:35 -0500

    Made UFO Utilities more accessible

--- UFO_Part_Deux/UFO_Utility.cpp
+++ UFO_Part_Deux/UFO_Utility.cpp
@@ -1,36 +1,16 @@
-#pragma once
-
-////////////////////////////////
-///// DEBUG PRINTING ///////////
-////////////////////////////////
-#ifndef ARDUINO
-	#define PC_DEBUG
-#else
-	#include <Arduino.h>
-#endif
-
-#ifdef PC_DEBUG
-	#include <iostream>
-	#include <String>
-	using namespace std;
-#endif
+#include "UFO_Utility.h"
 
 #ifdef PC_DEBUG
 void DebugPrint(std::string myString)
 {
-	cout << myString << endl;
+  cout << myString << endl;
 }
 #else
 void DebugPrint(char* myString)
 {
-	Serial.println(myString);
+  Serial.println(myString);
 }
 #endif
-
-////////////////////////////////////
-/////// Control Functions //////////
-///////////////////////////////////
-#include "UFO_Types.h"
 
 int lerp(int a, int b, float t) {
   return a + (b - a)*t; 
